6783f3afdd2a6eec932c1336c65c605bae3bc8f0
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-30 15:45:01 -0300

    Setup created

---
+++ Assets/Scenes/Scripts/Heroes.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroes : MonoBehaviour
+{
+    protected int lifeQuantity;
+    protected float velocity;
+    protected float damage;
+
+    protected void Attack() { }
+    protected void Die() { }
+    protected void Revive() { }
+    protected void Movement() { }
+    protected void Pick() { }
+}
