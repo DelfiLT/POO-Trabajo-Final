e2b1af8b5406c6b2352f965535a944aa70cbdaa3
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-07-04 00:55:23 -0300

    Enemy archer fixes

--- Assets/Scenes/Scripts/Enemies/EnemyArcher.cs
+++ Assets/Scenes/Scripts/Enemies/EnemyArcher.cs
@@ -19,11 +19,16 @@ public class EnemyArcher : Enemies
 
     void Update()
     {
-        distanceFromPlayer1 = Vector2.Distance(player1Transform.position, transform.position);
-        distanceFromPlayer2 = Vector2.Distance(player2Transform.position, transform.position);
+        if (player1Transform != null)
+        {
+            distanceFromPlayer1 = Vector2.Distance(player1Transform.position, transform.position);
+        }
+        if (player2Transform != null)
+        {
+            distanceFromPlayer2 = Vector2.Distance(player2Transform.position, transform.position);
+        }
 
-
-        if (distanceFromPlayer1 < lineOfSite && distanceFromPlayer1 > shootingRange && distanceFromPlayer1 < distanceFromPlayer2)
+        if (distanceFromPlayer1 < lineOfSite && distanceFromPlayer1 > shootingRange && distanceFromPlayer1 < distanceFromPlayer2 && player1Transform != null)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player1Transform.position, velocity * Time.deltaTime);
         }
@@ -33,7 +38,7 @@ public class EnemyArcher : Enemies
             nextFireTime = Time.time + fireRate;
         }
 
-        if (distanceFromPlayer2 < lineOfSite && distanceFromPlayer2 > shootingRange && distanceFromPlayer2 < distanceFromPlayer1)
+        if (distanceFromPlayer2 < lineOfSite && distanceFromPlayer2 > shootingRange && distanceFromPlayer2 < distanceFromPlayer1 && player2Transform != null)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player2Transform.position, velocity * Time.deltaTime);
         }
@@ -41,6 +46,33 @@ public class EnemyArcher : Enemies
         {
             Instantiate(bullet, bulletParent.transform.position, quaternion.identity);
             nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Die();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("lance"))
+        {
+            int lanceDamage = GameObject.FindGameObjectWithTag("Player1").GetComponent<meleeplayer>().lanceDamage;
+            hp = hp - lanceDamage;
+        }
+        if (collision.gameObject.CompareTag("Arrow"))
+        {
+            int arrowDamage = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerArcher>().arrowDamage;
+            hp = hp - arrowDamage;
+        }
+    }
+
+    protected override void Die()
+    {
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
